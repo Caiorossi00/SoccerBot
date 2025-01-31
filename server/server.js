f2a07a7bb13eab3efb7d472c24ce1117f87d64ce
Caiorossi00 <caio_rossi56@hotmail.com>
2025-01-31 16:30:48 -0300

    first commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
@@ -9,46 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "*" }));
 
-app.get("/", (req, res) => {
-  res.send("Servidor do FutebolChatbot está funcionando!");
-});
-
-app.get("/api/leagues", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api-football-v1.p.rapidapi.com/v3/leagues",
-      {
-        headers: {
-          "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-          "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-        },
-      }
-    );
-
-    if (response.data && response.data.response) {
-      res.json(response.data.response);
-    } else {
-      res.status(404).json({ message: "Nenhuma liga encontrada." });
-    }
-  } catch (error) {
-    console.error("Erro ao buscar ligas", error);
-    res.status(500).json({ message: "Erro ao buscar ligas da API de futebol" });
-  }
-});
-
-app.get("/api/games/:leagueId", async (req, res) => {
-  const leagueId = req.params.leagueId;
-  const date = new Date().toISOString().split("T")[0];
-
+const getGamesByLeague = async (leagueId, season) => {
   try {
     const response = await axios.get(
       "https://api-football-v1.p.rapidapi.com/v3/fixtures",
       {
-        params: {
-          league: leagueId,
-          season: 2025,
-          date: date,
-        },
+        params: { league: leagueId, season: season },
         headers: {
           "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
           "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
@@ -56,16 +21,40 @@ app.get("/api/games/:leagueId", async (req, res) => {
       }
     );
 
-    if (response.data && response.data.response) {
-      res.json(response.data.response);
-    } else {
-      res
-        .status(404)
-        .json({ message: "Nenhum jogo encontrado para essa liga." });
-    }
+    return response.data.response;
   } catch (error) {
-    console.error("Erro ao buscar jogos", error);
-    res.status(500).json({ message: "Erro ao buscar dados da API de futebol" });
+    console.error(
+      "Erro ao buscar jogos:",
+      error.response?.data || error.message
+    );
+    return [];
+  }
+};
+
+app.get("/api/games/:leagueId", async (req, res) => {
+  const leagueId = req.params.leagueId;
+  const season = 2024;
+  const date = new Date().toISOString().split("T")[0];
+
+  try {
+    const games = await getGamesByLeague(leagueId, season);
+
+    if (games.length === 0)
+      return res
+        .status(404)
+        .json({ message: "Nenhum jogo encontrado para hoje." });
+
+    const gamesToday = games.filter((game) =>
+      game.fixture.date.startsWith(date)
+    );
+
+    res.json(gamesToday);
+  } catch (error) {
+    console.error(
+      "Erro ao buscar jogos:",
+      error.response?.data || error.message
+    );
+    res.status(500).json({ message: "Erro ao buscar jogos." });
   }
 });
 
