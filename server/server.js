4aa1e116f88d7bd3e1377aa825301309c916758f
Caiorossi00 <caio_rossi56@hotmail.com>
2025-02-04 11:30:03 -0300

    endpoint pra todas as ligas

--- server/server.js
+++ server/server.js
@@ -8,6 +8,52 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "*" }));
 
+const getLeagues = async () => {
+  try {
+    const response = await axios.get(
+      "https://api-football-v1.p.rapidapi.com/v3/leagues",
+      {
+        headers: {
+          "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+          "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+        },
+      }
+    );
+
+    return response.data.response;
+  } catch (error) {
+    console.error(
+      "Erro ao buscar ligas:",
+      error.response?.data || error.message
+    );
+    return [];
+  }
+};
+
+app.get("/api/leagues", async (req, res) => {
+  try {
+    const leagues = await getLeagues();
+
+    if (!leagues || leagues.length === 0) {
+      return res.status(404).json({ message: "Nenhuma liga encontrada." });
+    }
+
+    const formattedLeagues = leagues.map((league) => ({
+      id: league.league.id,
+      nome: league.league.name,
+      país: league.country.name,
+    }));
+
+    res.json(formattedLeagues);
+  } catch (error) {
+    console.error(
+      "Erro ao buscar ligas:",
+      error.response?.data || error.message
+    );
+    res.status(500).json({ message: "Erro ao buscar ligas." });
+  }
+});
+
 const getGamesByLeague = async (leagueId, season) => {
   try {
     const response = await axios.get(
@@ -32,20 +78,21 @@ const getGamesByLeague = async (leagueId, season) => {
 };
 
 app.get("/api/games/:leagueId", async (req, res) => {
-  const leagueId = req.params.leagueId;
+  const { leagueId } = req.params;
   const season = 2024;
-  const date = new Date().toISOString().split("T")[0];
+  const today = new Date().toISOString().split("T")[0];
 
   try {
     const games = await getGamesByLeague(leagueId, season);
 
-    if (games.length === 0)
+    if (!games || games.length === 0) {
       return res
         .status(404)
         .json({ message: "Nenhum jogo encontrado para hoje." });
+    }
 
     const gamesToday = games.filter((game) =>
-      game.fixture.date.startsWith(date)
+      game.fixture.date.startsWith(today)
     );
 
     res.json(gamesToday);
